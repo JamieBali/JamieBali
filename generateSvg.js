636a7001b289311949abd1a9c52b4511db39744a
James (Jamie) Bali <92593423+JamieBali@users.noreply.github.com>
2024-10-03 23:47:26 +0100

    Update generateSvg.js

--- generateSvg.js
+++ generateSvg.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const svgContent = `
 <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
-  <rect width="100" height="100" style="fill:blue"/>
+  <rect width="100" height="100" style="fill:green"/>
 </svg>
 `;
 
